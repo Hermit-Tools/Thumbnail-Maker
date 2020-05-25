649e184620573ddd650f0728e05a53974c07a075
mmaismma <mmaismma@hotmail.com>
2020-05-25 13:59:41 +0530

    Beautified file

--- Hermits' Thumbnail Maker/cubfan135/script.js
+++ Hermits' Thumbnail Maker/cubfan135/script.js
@@ -7,14 +7,13 @@ const getImage = document.getElementById('getImage');
 getImage.addEventListener('click', finishEditing.bind())
 
 function editOnCanvas(event) {
-    let img = new Image();
-    img.addEventListener('load', function () {
-        ctx.drawImage(img, 0, 0, 1920, 1280);
-
+    let bgImage = new Image();
+    bgImage.addEventListener('load', () => {
+        ctx.drawImage(bgImage, 0, 0, 1920, 1280);
         episodeNum();
     }, false);
 
-    img.src = URL.createObjectURL(event.target.files[0]);
+    bgImage.src = URL.createObjectURL(event.target.files[0]);
 }
 
 function episodeNum() {
@@ -23,6 +22,7 @@ function episodeNum() {
     ctx.lineWidth = 4;
     ctx.font = '300px Verdana';
     ctx.textBaseline = 'bottom';
+
     ctx.fillText('30', 20, 1270);
     ctx.strokeText('30', 20, 1270);
     ctx.fill();
@@ -32,5 +32,4 @@ function episodeNum() {
 function finishEditing() {
     let image = canvas.toDataURL();
     output.src = image;
-    console.log(',,a');
 }
