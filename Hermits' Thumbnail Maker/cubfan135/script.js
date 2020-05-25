316e7d85b9f4a706b95312c365ade84d60b6a78c
mmaismma <mmaismma@hotmail.com>
2020-05-25 13:56:29 +0530

    Made new function for rendering episode number

--- Hermits' Thumbnail Maker/cubfan135/script.js
+++ Hermits' Thumbnail Maker/cubfan135/script.js
@@ -11,26 +11,26 @@ function editOnCanvas(event) {
     img.addEventListener('load', function () {
         ctx.drawImage(img, 0, 0, 1920, 1280);
 
-        ctx.fillStyle = '#fff';
-        ctx.strokeStyle = '#000';
-        ctx.lineWidth = 4;
-
-        ctx.font = '300px Verdana';
-        ctx.textBaseline = 'bottom';
-
-        ctx.fillText('30', 20, 1270);
-        ctx.strokeText('30', 20, 1270);
-
-        ctx.fill();
-        ctx.stroke();
+        episodeNum();
     }, false);
 
     img.src = URL.createObjectURL(event.target.files[0]);
+}
+
+function episodeNum() {
+    ctx.fillStyle = '#fff';
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 4;
+    ctx.font = '300px Verdana';
+    ctx.textBaseline = 'bottom';
+    ctx.fillText('30', 20, 1270);
+    ctx.strokeText('30', 20, 1270);
+    ctx.fill();
+    ctx.stroke();
 }
 
 function finishEditing() {
     let image = canvas.toDataURL();
     output.src = image;
     console.log(',,a');
-
 }
