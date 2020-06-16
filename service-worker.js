fcc8116b9dd365487d76007deecdce60c8188dc6
mmaismma <mmaismma@hotmail.com>
2020-06-16 14:51:26 +0530

    Finalised service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,34 @@
-var CACHE_NAME = 'ht-cache-v1';
+var cacheName = 'ht-cache-v1';
 var urlsToCache = [
-  '/',
-  'style.css',
-  'script.js'
+    '/',
+    './style.css',
+    './script.js',
+    './manifest.webmanifest',
+    './cubfan135/index.html',
+    './cubfan135/style.css',
+    './cubfan135/script.js',
 ];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
 });
