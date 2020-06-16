a42a923782df39ef21a336f21c2392dca58be859
mmaismma <mmaismma@hotmail.com>
2020-06-16 12:42:06 +0530

    Intialised adding service worker

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+var CACHE_NAME = 'ht-cache-v1';
+var urlsToCache = [
+  '/',
+  'style.css',
+  'script.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
