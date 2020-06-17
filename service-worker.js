7c9368111119019788c5332e8eaf136b21708c7e
mmaismma <mmaismma@hotmail.com>
2020-06-17 12:10:15 +0530

    Page now works offline

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,16 @@
-var cacheName = 'ht-cache-v1';
-var urlsToCache = [
-    '/',
-    './style.css',
-    './script.js',
-    './manifest.webmanifest',
-    './cubfan135/index.html',
-    './cubfan135/style.css',
-    './cubfan135/script.js',
-];
+const CACHE = "ht-offline";
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((r) => {
-            console.log('[Service Worker] Fetching resource: ' + e.request.url);
-            return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
-                    cache.put(e.request, response.clone());
-                    return response;
-                });
-            });
-        })
-    );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
