51f93537a7f41b96b20ccac030f379b15c04cb5a
mmaismma <mmaismma@hotmail.com>
2020-07-06 13:31:06 +0530

    Match starturl(manifest) w/ scope(service worker)

--- cubfan135/service-worker.js
+++ cubfan135/service-worker.js
@@ -7,7 +7,6 @@ self.addEventListener('install', function(event) {
           'script.js',
           'manifest.webmanifest',
           '/hc7logobydnator.png',
-          'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap',
           'https://fonts.googleapis.com/icon?family=Material+Icons'
         ]),
         console.log('cached');        
