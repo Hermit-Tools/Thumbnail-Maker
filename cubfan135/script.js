cf09a1b7b8933543d656157e639d5b1aa82d6c7c
Mayank Arya <mmaismma@hotmail.com>
2020-07-08 17:37:31 +0530

    Tweaked settings
    Matched HC7 logo and epNum style with what cub uses

--- cubfan135/script.js
+++ cubfan135/script.js
@@ -1,63 +1,71 @@
-const bgInput = document.getElementById('bgInput');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const epNumSelector = document.getElementById('epNumSelector');
-const hcLogoToggler = document.getElementById('hcLogoToggler');
-const previewText = document.getElementById('preview-text');
+const bgInput = document.getElementById("bgInput");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const epNumSelector = document.getElementById("epNumSelector");
+const hcLogoToggler = document.getElementById("hcLogoToggler");
+const previewText = document.getElementById("preview-text");
+const downloader = document.getElementById("downloader");
 
 function editOnCanvas() {
-    let bgImage = new Image();
-    bgImage.addEventListener('load', () => {
-        ctx.drawImage(bgImage, 0, 0, 1920, 1280);
-        hcLogo();
-        episodeNum();
-    }, false);
+  let bgImage = new Image();
+  bgImage.addEventListener(
+    "load",
+    () => {
+      ctx.drawImage(bgImage, 0, 0, 1920, 1080);
+      hcLogo();
+      episodeNum();
+    },
+    false
+  );
 
-    bgImage.src = URL.createObjectURL(bgInput.files[0]);
+  bgImage.src = URL.createObjectURL(bgInput.files[0]);
 }
 
 function episodeNum() {
-    let epNum = epNumSelector.value;
+  let epNum = epNumSelector.value;
 
-    ctx.fillStyle = '#fff';
-    ctx.strokeStyle = '#000';
-    ctx.lineWidth = 7;
-    ctx.font = 'bold 300px Tahoma';
-    ctx.textBaseline = 'bottom';
+  ctx.fillStyle = "#fff";
+  ctx.strokeStyle = "#000";
+  ctx.lineWidth = 15;
+  ctx.font = "1000 401px Tahoma";
+  ctx.textBaseline = "bottom";
 
-    ctx.fillText(epNum, 20, 1270);
-    ctx.strokeText(epNum, 20, 1270);
-    ctx.fill();
-    ctx.stroke();
+  ctx.fillText(epNum, 25, 1100);
+  ctx.strokeText(epNum, 25, 1100);
+  ctx.fill();
+  ctx.stroke();
 }
 
 function hcLogo() {
-    if (hcLogoToggler.checked) {
-        let hc7Logo = new Image()
-        hc7Logo.addEventListener('load', () => {
-            ctx.drawImage(hc7Logo, 90, 15, 1800, 221.54)
-        })
-        hc7Logo.src = ("https://hermit-tools.github.io/Thumbnail-Maker/hc7logobydnator.png");
-        hc7Logo.crossOrigin = 'Anonymous';
-    }
+  if (hcLogoToggler.checked) {
+    let hc7Logo = new Image();
+    hc7Logo.addEventListener("load", () => {
+      ctx.drawImage(hc7Logo, 15.7, 40, 1887.5, 244);
+    });
+    hc7Logo.src =
+      "https://hermit-tools.github.io/Thumbnail-Maker/hc7logobydnator.png";
+    hc7Logo.crossOrigin = "Anonymous";
+  }
 }
 
 function finishEditing() {
-    document.getElementById("downloader").download = `HC7-Ep${epNumSelector.value}-CubsContraptionByHermitTools`;
-    document.getElementById("downloader").href = canvas.toDataURL("image/png").replace('data:image/png', 'data:concorp>sahara');
+  downloader.download = `HC7-Ep${epNumSelector.value}-CubsContraptionByHermitTools.jpg`;
+  downloader.href = canvas
+    .toDataURL("image/png")
+    .replace("data:image/png", "data:concorp>sahara");
 }
 
 function process() {
-    ctx.fillStyle = '#fff'
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    if (bgInput.files.length != 0) {
-        editOnCanvas();
-    } else {
-        hcLogo();
-        episodeNum();
-    }
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  if (bgInput.files.length != 0) {
+    editOnCanvas();
+  } else {
+    hcLogo();
+    episodeNum();
+  }
 }
-//cookies stuff
+// Cookies Stuff
 let epNumValueFromCookie;
 
 downloader.addEventListener("click", () => {
@@ -69,14 +77,10 @@ if (document.cookie.length === 0) {
 } else {
   epNumValueFromCookie = document.cookie.split("=")[1];
   epNumSelector.value = Number(epNumValueFromCookie) + 1;
-};
+}
 // Service Worker
-console.log('wonder');
-if('serviceWorker' in navigator) {
-    console.log('sw register 1');
-    window.addEventListener('load', () => {
-        console.log('sw register 2');
-        navigator.serviceWorker.register('service-worker.js');
-        console.log('sw registered');
-    })
-};
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("service-worker.js");
+  });
+}
