04b3377181b709f9cabab4547c4b5d751fce0e88
mmaismma <mmaismma@hotmail.com>
2020-08-18 15:41:40 +0530

    🖋Changed how service-worker.js works 1. Roboto font is now not loaded 2. Changed body font

--- Source/cubfan135/service-worker.js
+++ Source/cubfan135/service-worker.js
@@ -1,20 +1,43 @@
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open("Cub'sContraption").then(function(cache) {
-      return cache.addAll([
-        "./style.css",
-        "./script.js",
-        "../Resources/Hermitcraft Logos/HC7 Logo.png",
-        "https://fonts.googleapis.com/icon?family=Material+Icons"
-      ]);
-    })
-  );
-});
+function precache() {
+  caches.open("CubsContraption").then(function (cache) {
+    return cache.addAll([
+      "./style.css",
+      "./script.js",
+      "./index.html",
+      "./manifest.webmanifest",
+      "../Resources/Hermitcraft Logos/HC7 Logo.png",
+      "https://fonts.googleapis.com/icon?family=Material+Icons"
+    ]);
+  })
+}
+
+function fromCache(request) {
+  return caches.open("CubsContraption").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+function update(request) {
+  return caches.open("CubsContraption").then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+}
+
+self.addEventListener("install", function (event) {
+  console.log("SW is installing.");
+  event.waitUntil(precache())
+})
 
 self.addEventListener("activate", event => {
   console.log("Inside the activate handler:", event);
 });
 
 self.addEventListener("fetch", event => {
-  console.log("Inside the fetch handler:", event);
+  console.log("SW is being fetched.");
+  event.respondWith(fromCache(event.request));
+  event.respondWith(updateCache(event.request));
 });
