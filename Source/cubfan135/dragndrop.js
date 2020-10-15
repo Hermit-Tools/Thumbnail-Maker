c9ac3dbda01ce67980496ca955c6aa966df160d6
Norbert Sándor <96.sandor.norbert@gmail.com>
2020-10-15 16:16:33 +0200

    drag n drop functionality

--- Source/cubfan135/dragndrop.js
+++ Source/cubfan135/dragndrop.js
@@ -1,20 +1,59 @@
 (function($) {
     var body = $('body'),
-        preview = body.find('#preview-area #preview-text'),
-        previewHTML = ' <i class="material-icons-outlined">remove_red_eye</i> Preview';
+        dropZone = body.find('#drop-zone'),
+        displayFlex = 'block',
+        displayNone = 'none',
+        dropInput = body.find('#dropInput');
 
-        body.on('dragover', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            preview.addClass('drop');
-            preview.html('DROP FILE HERE');
-        });
+    // WORKING
+    function preventDef(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
 
-        body.on('dragleave', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            preview.removeClass('drop');
-            preview.html(previewHTML);
-        });
+
+    var lastTarget = null;
+
+    function isFile(evt) {
+        var dt = evt.dataTransfer;
+
+        for (var i = 0; i < dt.types.length; i++) {
+            if (dt.types[i] === "Files") {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    window.addEventListener("dragenter", function (e) {
+        if (isFile(e)) {
+            lastTarget = e.target;
+            $("#dropzone").css('visibility','visible');
+            $("#dropzone").css('opacity', '1');
+            $("#dropzone h1").css('font-size', '3em');
+            $("#bgInput").addClass('dropInput');
+        }
+    });
+
+    window.addEventListener("dragleave", function (e) {
+        e.preventDefault();
+        if (e.target === lastTarget || e.target === document) {
+            $("#dropzone").css('visibility', 'hidden');
+            $("#dropzone").css('opacity', '0');
+            $("#dropzone h1").css('font-size', '0em');
+        }
+    });
+
+    window.addEventListener("dragover", function (e) {
+        preventDef(e);
+    });
+
+    window.addEventListener("drop", function (e) {
+        //preventDef(e);
+        $("#dropzone").css('visibility', 'hidden');
+        $("#dropzone").css('opacity', '0');
+        $("#dropzone h1").css('font-size', '0em');
+        $("#bgInput").removeClass('dropInput');
+    });
 
 })(jQuery);
